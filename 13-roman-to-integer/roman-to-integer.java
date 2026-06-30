8920460ee09bbe8c7e57f29e0a0672c918aab1c0
RuchitPahadia <148325419+RuchitPahadia@users.noreply.github.com>
2026-06-30 19:31:36 +0530

    Time: 2 ms (100.00%) | Memory: 46.6 MB (49.42%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,32 +1,32 @@
 class Solution {
     public int romanToInt(String s) {
-        int res=0;
-        String[] s1=s.split("");
-        int[] nums = new int[s1.length];
-        for(int i = 0;i<nums.length;i++){
-            nums[i]=valof(s1[i]);
-        }
-        for(int i = 0;i<nums.length-1;i++){
-            if(nums[i]<nums[i+1]){
-                res-=nums[i];
-            }
-            else{
-                res+=nums[i];
+        int res = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            int curr = valof(s.charAt(i));
+            int next = valof(s.charAt(i + 1));
+
+            if (curr < next) {
+                res -= curr;
+            } else {
+                res += curr;
             }
         }
-        res+=nums[nums.length-1];
-        
+
+        res += valof(s.charAt(s.length() - 1));
+
         return res;
     }
-    public int valof(String c){
+
+    public int valof(char c) {
         return switch (c) {
-            case "M" -> 1000;
-            case "D" -> 500;
-            case "C" -> 100;
-            case "L" -> 50;
-            case "X" -> 10;
-            case "V" -> 5;
-            case "I" -> 1;
+            case 'M' -> 1000;
+            case 'D' -> 500;
+            case 'C' -> 100;
+            case 'L' -> 50;
+            case 'X' -> 10;
+            case 'V' -> 5;
+            case 'I' -> 1;
             default -> 0;
         };
     }
