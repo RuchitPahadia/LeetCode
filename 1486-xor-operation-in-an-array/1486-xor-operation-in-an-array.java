2da8feb5f400ecd1db156f007b2cb2752dbbec3b
Ruchit Pahadia <148325419+RuchitPahadia@users.noreply.github.com>
2026-07-31 17:49:19 +0530

    Time: 0 ms (100%), Space: 42 MB (76.48%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int res=0;
+        for(int i=0;i<n;i++){
+            int t=start + (2 * i);
+            res=res^t;
+        }
+        return res;
+    }
+}
