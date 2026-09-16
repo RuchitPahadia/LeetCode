039f34c554ddf284786c0d1fd771e8fdcfccdcc0
Ruchit Pahadia <148325419+RuchitPahadia@users.noreply.github.com>
2026-09-17 02:37:55 +0530

    Time: 1 ms (95.89%) | Memory: 46.9 MB (41.10%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,21 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
-
- //
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode node) {
         List<List<Integer>> res = new ArrayList<>();
@@ -27,23 +9,23 @@ class Solution {
         q.add(node);//add first element
         
         while (!q.isEmpty()) {
-            int size = q.size();
+            int size = q.size();//to add elements of curr lwvwl from bottom and remove old from bottom
 
-            List<Integer> level = new ArrayList<>();//create a array to store the elements
+            List<Integer> level = new ArrayList<>();//create a array to store the elements in a level
 
             for(int i=0;i<size;i++){
-                TreeNode root = q.poll();
+                TreeNode root = q.poll();//go to next node of prev level 
 
-                level.add(root.val);
+                level.add(root.val);//add the node of this level
 
-                if(root.left!=null){
+                if(root.left!=null){//add left node 
                     q.add(root.left);
                 }
-                if(root.right!=null){
+                if(root.right!=null){//add right node
                     q.add(root.right);
                 }
             }
-            res.add(level);
+            res.add(level);//update result
         }
         return res;
     }
